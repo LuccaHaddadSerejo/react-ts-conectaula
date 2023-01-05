933415095db67413496995bcb137389457d084fc
Lucca Haddad <hslucca@gmail.com>
2023-01-05 19:45:53 -0300

    feat: userContext logic and typing. all api requisitions and protected routes logic

--- src/contexts/userContext/types.ts
+++ src/contexts/userContext/types.ts
@@ -1,5 +1,66 @@
-export interface iUserProviderValue {}
+import { iMessagesObj } from "../../services/api";
+
+export interface iUserProviderValue {
+  submitLogin: (data: iFormLoginData) => void;
+  submitRegisterStudent: (data: iFormRegisterStudentData) => void;
+  submitRegisterTeacher: (data: iFormRegisterTeacherData) => void;
+  submitEditProfile: (data: iUserState, id: number) => void;
+  submitMessage: (data: iMessagesObj) => void;
+  deleteUser: (id: number) => void;
+  logout: () => void;
+  globalLoading: boolean;
+  dashboardLoading: boolean;
+  user: null | iUserState;
+}
 
 export interface iUserProviderProps {
   children: React.ReactNode;
+}
+
+export interface iUserState {
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  age: number;
+  type: string;
+  photo_url?: string;
+  student_age?: string;
+  schooling?: string;
+  school_year?: string;
+  bio?: string;
+  school_year_preference?: string[];
+  grades?: string[];
+}
+
+export interface iFormLoginData {
+  email: string;
+  password: string;
+}
+
+export interface iLoginRegisterResponse {
+  accessToken: string;
+  user: iUserState;
+}
+
+export interface iFormRegisterTeacherData {
+  name: string;
+  email: string;
+  age: number;
+  password: string;
+  bio: string;
+  confirm_password?: string;
+  photo_url: string;
+  school_year_preference: string[];
+  grades: string[];
+  type: string;
+}
+
+export interface iFormRegisterStudentData {
+  name: string;
+  email: string;
+  age: number;
+  password: string;
+  confirm_password?: string;
+  type: string;
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,91 @@
-export {};
+import axios from "axios";
+import { iUserState } from "../contexts/userContext/types";
+
+const staticURL = "https://api-conectaula.onrender.com";
+
+export interface iGradesObj {
+  id: number;
+  schooling: string;
+  specific_grades: string[];
+}
+
+export interface iMessagesObj {
+  id?: number;
+  teacher_id: number;
+  student_id: number;
+  message: string;
+}
+
+export const api = axios.create({
+  baseURL: "https://api-conectaula.onrender.com",
+  timeout: 10000,
+});
+
+export const getGradeByid = async (id: number) => {
+  try {
+    const response = await api.get<iGradesObj[]>(`${staticURL}/grade?id=${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getAllGrades = async () => {
+  try {
+    const response = await api.get<iGradesObj[]>(`${staticURL}/grades`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getAllTeachers = async () => {
+  try {
+    const response = await api.get<iUserState[]>(
+      `${staticURL}/users?type=teacher`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getAllMessages = async () => {
+  try {
+    const response = await api.get<iMessagesObj[]>(`${staticURL}/messages`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getMessageById = async (id: number) => {
+  try {
+    const response = await api.get<iMessagesObj[]>(
+      `${staticURL}/messages/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getAllStudents = async () => {
+  try {
+    const response = await api.get<iUserState[]>(
+      `${staticURL}/users?type=student`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getStudentById = async (id: number) => {
+  try {
+    const response = await api.get<iUserState[]>(`${staticURL}/users/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
