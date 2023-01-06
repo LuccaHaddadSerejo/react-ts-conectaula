f10204bf08da22bdd601037f1b443d56aa7c740a
Lucca Haddad <hslucca@gmail.com>
2023-01-06 14:54:37 -0300

    fix: links path in landing page

--- src/pages/Landingpage/index.tsx
+++ src/pages/Landingpage/index.tsx
@@ -1,27 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/img/logo.svg"
-import landingImg from "../../assets/img/landingImg.svg"
+import logo from "../../assets/img/logo.svg";
+import landingImg from "../../assets/img/landingImg.svg";
 import { StyledLandingPage } from "./style";
 
 const Landingpage = () => {
   return (
     <StyledLandingPage>
       <div className="dashboardContent">
-        <img src={logo} alt="Logo com nome e uma imagem representando pessoas conectadas" />
-        <p><span>Aprenda</span> ou <span>ensine</span> de qualquer lugar do Brasil com o <span>ConectAulas</span></p>
+        <img
+          src={logo}
+          alt="Logo com nome e uma imagem representando pessoas conectadas"
+        />
+        <p>
+          <span>Aprenda</span> ou <span>ensine</span> de qualquer lugar do
+          Brasil com o <span>ConectAulas</span>
+        </p>
         <p>Escolha se você é professor ou aluno e cadastre-se!</p>
         <div className="dashboardSelection">
-          <Link to={"/dashboard/student/:name"}>Aluno</Link>
-          <Link to={"/dashboard/teacher/:name"}>Professor</Link>
+          <Link to={"/register/student"}>Aluno</Link>
+          <Link to={"/register/teacher"}>Professor</Link>
         </div>
-        <p>Já possui cadastro? Faça seu login <span><Link to={"/login"}>aqui</Link></span></p>
+        <p>
+          Já possui cadastro? Faça seu login{" "}
+          <span>
+            <Link to={"/login"}>aqui</Link>
+          </span>
+        </p>
       </div>
       <div className="dashboardImg">
-        <img src={landingImg} alt="Foto de estudante tendo aula pelo computador" />
+        <img
+          src={landingImg}
+          alt="Foto de estudante tendo aula pelo computador"
+        />
       </div>
     </StyledLandingPage>
-    
   );
 };
 
