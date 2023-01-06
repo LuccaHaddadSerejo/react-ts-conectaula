11f552bebb7dab3c5db2fdf14388f0d33733b20c
Isadora <dorisperdigao@gmail.com>
2023-01-06 07:16:12 -0300

    feat: creates landing page structure and links

--- src/pages/Landingpage/style.ts
+++ src/pages/Landingpage/style.ts
@@ -1,1 +1,11 @@
-export {};
+import styled from "styled-components";
+
+export const StyledLandingPage = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    .dashboardSelection {
+        display: flex;
+        flex-direction: column;
+    }
+`
