413ba8bcf1a4f7862b4f1c26459bf10a3703236a
Bryan <bryanbatista79688@gmail.com>
2023-01-07 12:06:31 -0300

    feat: dashboard containers, gradeFilter component, header files

---
+++ src/components/GradeFilter/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledSelect = styled.select`
+  width: 100%;
+  max-width: 220px;
+  height: 35px;
+  border-radius: 8px;
+  border: 2px solid var(--color-gray300);
+
+  &, & > option {
+    color: var(--color-gray300);
+    font-size: 14px;
+    font-weight: bold;
+  }
+`;
+
+export default StyledSelect;
---
+++ src/components/HeaderContainer/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HeaderStyledContainer = styled.div`
+
+  width: 100%;
+  height: 100%;
+  margin: 0 30px;
+
+`;
+
+export default HeaderStyledContainer;
