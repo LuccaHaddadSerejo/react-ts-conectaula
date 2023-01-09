f34f6e6c5b795a7cac3767f8129b075c0c955f83
julianairana <julianairana@hotmail.com>
2023-01-09 18:32:17 -0300

    refactor/header and card component and style

--- src/components/MainContainer/style.ts
+++ src/components/MainContainer/style.ts
@@ -3,7 +3,10 @@ import styled from "styled-components";
 const MainStyledContainer = styled.div`
   width: 100%;
   height: 100%;
-  padding: 0 30px;
+  padding: 70px 15px;
+  background-image: linear-gradient(240deg, var(--color-secondary) 0%, var(--color-primary) 100%) ;
+  display: flex;
+  justify-content: center;
 `;
 
 export default MainStyledContainer;
--- src/pages/Dashboard teacher/index.tsx
+++ src/pages/Dashboard teacher/index.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import EditProfileTeacher from "../../components/EditProfileTeacher";
+import Header from "../../components/Header";
+import MainStyledContainer from "../../components/MainContainer/style";
+import { StyledContent } from "./style";
+import { ModalEditTeacher } from "../../components/ModalEditTeacher";
+
+export interface iModalProps {
+  OpenModal: (boolean: boolean) => void;
+  modalIsOpen: boolean;
+  setModalIsOpen: (value: boolean) => void;
+}
+
+export interface iEditProps {
+  OpenModal: (boolean: boolean) => void;
+}
 
 const DashBoardTeacher = () => {
+  const [modalIsOpen, setModalIsOpen] = useState<iModalProps | boolean>(false);
+
+  const OpenModal = (boolean: boolean) => {
+    setModalIsOpen(boolean)
+  }
+
   return (
     <>
-  <h1>DashBoardTeacher</h1>
- <EditProfileTeacher/>
+    {
+      modalIsOpen === true && 
+    <ModalEditTeacher OpenModal={OpenModal}/>
+    }
+      <Header/>
+      <MainStyledContainer>
+      <StyledContent>
+        <div className="divDate">
+          <h2 className="h2Solicitation">Solicitações de alunos</h2>
+          <p>filtro</p>
+          <p>card</p> 
+        </div>
+        <div className="divEdit">
+      <EditProfileTeacher OpenModal={OpenModal}/>
+        </div>
+
+      </StyledContent>
+      </MainStyledContainer>
     </>
   ) 
 
