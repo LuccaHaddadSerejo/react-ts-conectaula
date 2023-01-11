2aed90696ff79fabf435f68790a95a44b40b019b
julianairana <julianairana@hotmail.com>
2023-01-11 14:03:32 -0300

    feat/modal delete page student, fix/card profile page student

--- src/components/EditProfileStudent/index.tsx
+++ src/components/EditProfileStudent/index.tsx
@@ -4,7 +4,7 @@ import { UserContext } from "../../contexts/userContext";
 import { Button } from "../Button";
 import editIcon from "../../assets/img/edit.svg";
 import trash from "../../assets/img/trash.svg";
-import { ModalDelet } from "../ModalDelet";
+import { ModalDelet } from "../ModalDeletStudent";
 import { iModalEditProps } from "../../pages/Dashboard student";
 
 
@@ -24,7 +24,7 @@ const EditProfileStudent = ({ OpenModalStudent }: iModalEditProps) => {
 
     return (
         <>
-        {modalDeletStudent && <ModalDelet closeModal={closeModal}/>}
+        
         <StyledEditProfile>
         <img src={user?.photo_url} alt="Foto de perfil" className="imgProfile"/>
         <h2>{user?.name}</h2>
@@ -33,6 +33,7 @@ const EditProfileStudent = ({ OpenModalStudent }: iModalEditProps) => {
         <Button type={"button"} onClick={() => OpenModalStudent(true)}>Perfil <span><img src={editIcon} alt=""></img></span></Button>
         <Button type={"button"}> <img src={trash} alt="" onClick={openModal}/></Button>
         </div>
+        {modalDeletStudent && <ModalDelet closeModal={closeModal}/>}
         </StyledEditProfile>
         </>
     )
--- src/components/ModalContainer/style.ts
+++ src/components/ModalContainer/style.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const StyledModalContainer = styled.div`
     width: 100%;
     height: 100%;
+    top: 0;
+    left: 0;
     position: fixed;
     display: flex;
     justify-content: center;
